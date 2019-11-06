f9802cd90168ac732d65b8fdeca81281fca5e0d9
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-06 16:39:31 +0100

    Update Demo scene

--- Demo.gd
+++ Demo.gd
@@ -1,9 +1,14 @@
 extends Spatial
 
+var door_open = false
+
 
 func _ready():
 	oat_interaction_signals.connect("oat_environment_item_selected", self, "notify", ["Selected: "])
 	oat_interaction_signals.connect("oat_environment_item_deselected", self, "notify", ["Deselected: "])
+	oat_interaction_signals.connect("oat_environment_item_activated", self, "notify", ["Activated: "])
+	
+	oat_interaction_signals.connect("oat_environment_item_activated", self, "activate")
 
 
 func notify(item_name, text):
@@ -13,3 +18,14 @@ func notify(item_name, text):
 
 func _on_Timer_timeout():
 	$Notification.text = ""
+
+
+func activate(item_name):
+	if item_name == "button_2":
+		if door_open:
+			$AnimationPlayer.play_backwards("open_door")
+		else:
+			$AnimationPlayer.play("open_door")
+		door_open = not door_open
+	if item_name == "button_1":
+		$AnimationPlayer.play("drop_crate")
