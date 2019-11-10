5cbbd049aca1f0b8dcc245c74020606bb323bd9e
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-10 17:39:26 +0100

    Add simple inventory item list

--- oat/Inventory.gd
+++ oat/Inventory.gd
@@ -1,18 +1,20 @@
-extends CenterContainer
+extends Control
 
 # TODO: read this from settings
 export var ROTATION_SENSITIVITY_X = 1.0
 export var ROTATION_SENSITIVITY_Y = 1.0
 
 const MAX_VERTICAL_ANGLE = 80
-onready var rotator = $ViewportContainer/Viewport/Inventory3D/Rotator
+onready var viewport = $CenterContainer/ViewportContainer/Viewport
+onready var rotator = $CenterContainer/ViewportContainer/Viewport/Inventory3D/Rotator
+onready var item_list = $ItemList
 # TODO: keep rotation value separately for each item?
 var current_angle_vertical = 0
 
 
 func _ready():
 	# Setting own_world here, otherwise 3D world will not be shown in Godot Editor
-	$ViewportContainer/Viewport.own_world = true
+	viewport.own_world = true
 	oat_interaction_signals.connect("oat_toggle_inventory", self, "toggle")
 	oat_interaction_signals.connect("oat_environment_item_obtained", self, "item_obtained")
 
@@ -30,7 +32,7 @@ func _input(event):
 			angle_vertical = clamp(angle_vertical, -MAX_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE)
 			var delta_angle_vertical = deg2rad(angle_vertical - current_angle_vertical)
 			current_angle_vertical = angle_vertical
-			
+		
 			rotator.rotate_object_local(Vector3(0, 1, 0), angle_horizontal)
 			rotator.rotate_x(delta_angle_vertical)
 	if Input.is_action_just_released("oat_inventory_item_rotation"):
@@ -53,3 +55,4 @@ func item_obtained(item_name):
 		obtained_item = obtained_item.pop_back()
 		obtained_item.get_parent().remove_child(obtained_item)
 		rotator.add_child(obtained_item)
+		item_list.add_icon_item(oat_interaction_signals.inventory_items_textures[item_name])
