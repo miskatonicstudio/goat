8225ec800a4710650c0e348bd9b455fb533df35d
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-10 13:35:41 +0100

    Add global game state and toggle_inventory signal

--- oat/signals/oat_interaction_signals.gd
+++ oat/signals/oat_interaction_signals.gd
@@ -2,5 +2,24 @@ extends Node
 
 signal oat_environment_item_selected (item_name)
 signal oat_environment_item_deselected (item_name)
-
 signal oat_environment_item_activated (item_name)
+
+signal oat_toggle_inventory (inventory_open)
+
+enum GameMode {
+	EXPLORING,
+	INVENTORY
+}
+
+export (GameMode) var game_mode = GameMode.EXPLORING
+
+
+# TODO: extract this to oat_event_handling.gd?
+func _input(event):
+	if Input.is_action_just_pressed("oat_toggle_inventory"):
+		if game_mode == GameMode.EXPLORING:
+			game_mode = GameMode.INVENTORY
+		elif game_mode == GameMode.INVENTORY:
+			game_mode = GameMode.EXPLORING
+		# TODO: send this signal also when inventory is open and Esc is pressed
+		emit_signal("oat_toggle_inventory", game_mode == GameMode.INVENTORY)
