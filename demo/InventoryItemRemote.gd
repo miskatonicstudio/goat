bf0addbdabbe24bfe38903678dbee96f41723989
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-12 17:22:02 +0100

    Add screen to Remote

--- demo/InventoryItemRemote.gd
+++ demo/InventoryItemRemote.gd
@@ -3,8 +3,15 @@ extends Spatial
 
 func _ready():
 	oat_interaction_signals.connect("oat_environment_item_activated", self, "item_activated")
+	$Screen.material_override.albedo_texture = $Viewport.get_texture()
 
 
 func item_activated(item_name):
 	if item_name == "remote_button":
 		$AnimationPlayer.play("press_button")
+		$Screen.material_override.albedo_color = Color.white
+		$Viewport/VideoPlayer.play()
+
+
+func _on_VideoPlayer_finished():
+	$Screen.material_override.albedo_color = Color.black
