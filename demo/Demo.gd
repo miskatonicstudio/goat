87596d0f4fa9b5b9899b9ae884df648a3693170e
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-12 13:52:36 +0100

    Update Demo scene

--- demo/Demo.gd
+++ demo/Demo.gd
@@ -10,16 +10,20 @@ func _ready():
 	oat_interaction_signals.connect("oat_environment_item_obtained", self, "notify", ["Obtained: "])
 	oat_interaction_signals.connect("oat_inventory_item_selected", self, "notify", ["Selected: "])
 	oat_interaction_signals.connect("oat_inventory_item_removed", self, "notify", ["Removed: "])
+	oat_interaction_signals.connect("oat_inventory_item_used", self, "notify", ["Used: "])
 	oat_interaction_signals.connect("oat_inventory_item_used_on_inventory", self, "notify2", ["Used: "])
 	oat_interaction_signals.connect("oat_inventory_item_replaced", self, "notify2", ["Replaced: "])
 	
 	oat_interaction_signals.connect("oat_environment_item_activated", self, "activate")
+	oat_interaction_signals.connect("oat_inventory_item_used", self, "use_item")
+	oat_interaction_signals.connect("oat_inventory_item_used_on_inventory", self, "use_item_on_inventory")
 	
 	# TODO: put it in a custom global file
 	oat_interaction_signals.inventory_items_textures["pen"] = load("res://demo/pen.png")
 	oat_interaction_signals.inventory_items_textures["ball"] = load("res://demo/ball.png")
 	oat_interaction_signals.inventory_items_textures["ball_on_a_stick"] = load("res://demo/ball_on_a_stick.png")
 	oat_interaction_signals.inventory_items_textures["cube"] = load("res://demo/cube.png")
+	oat_interaction_signals.inventory_items_textures["square"] = load("res://demo/square.png")
 	
 	oat_interaction_signals.inventory_items_models["pen"] = load("res://demo/InventoryItemPen.tscn")
 	oat_interaction_signals.inventory_items_models["ball"] = load("res://demo/InventoryItemBall.tscn")
@@ -27,8 +31,7 @@ func _ready():
 		"res://demo/InventoryItemBallOnAStick.tscn"
 	)
 	oat_interaction_signals.inventory_items_models["cube"] = load("res://demo/InventoryItemCube.tscn")
-	
-	oat_interaction_signals.connect("oat_inventory_item_used_on_inventory", self, "use_item_on_inventory")
+	oat_interaction_signals.inventory_items_models["square"] = load("res://demo/InventoryItemSquare.tscn")
 
 
 func notify(item_name, text):
@@ -44,9 +47,17 @@ func notify2(item_name1, item_name2, text):
 
 
 func use_item_on_inventory(item_name1, item_name2):
+	# TODO: move this logic to global singleton?
 	if item_name1 == "pen" and item_name2 == "ball" or item_name1 == "ball" and item_name2 == "pen":
 		oat_interaction_signals.emit_signal("oat_inventory_item_replaced", item_name2, "ball_on_a_stick")
 		oat_interaction_signals.emit_signal("oat_inventory_item_removed", item_name1)
+
+
+func use_item(item_name):
+	if item_name == "cube":
+		oat_interaction_signals.emit_signal("oat_inventory_item_replaced", item_name, "square")
+	if item_name == "square":
+		oat_interaction_signals.emit_signal("oat_inventory_item_removed", "square")
 
 
 func _on_Timer_timeout():
