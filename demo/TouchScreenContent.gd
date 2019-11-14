fed9729bdb704b99c13e476ed35366a18f11e16b
Paweł Fertyk <pfertyk@pfertyk.me>
2019-11-14 13:15:08 +0100

    Add touch screen content demo

---
+++ demo/TouchScreenContent.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var dot = $Dot
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_ButtonDown_pressed():
+	dot.rect_position.y += 10
+
+
+func _on_ButtonUp_pressed():
+	dot.rect_position.y -= 10
+
+
+func _on_ButtonRight_pressed():
+	dot.rect_position.x += 10
+
+
+func _on_ButtonLeft_pressed():
+	dot.rect_position.x -= 10
