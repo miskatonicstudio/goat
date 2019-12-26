e12ca2724b033cc49ac1ed5c9f05d5e7b251d5f0
Paweł Fertyk <pfertyk@pfertyk.me>
2019-12-26 21:48:46 +0100

    Add Desk scene

---
+++ demo/workshop/Desk.gd
@@ -0,0 +1,32 @@
+extends Spatial
+
+var computer_powered_up = false
+
+
+func _ready():
+	goat.connect("interactive_item_activated", self, "item_activated")
+	goat.connect("inventory_item_used_on_environment", self, "item_used_on_environment")
+
+
+func item_activated(item_name, _position):
+	if item_name == "generator":
+		var computer_screen_on = load(
+			"res://demo/workshop/materials/computer_screen_on.material"
+		)
+		# Set computer screen's material (surface: 1)
+		$Desk/Monitor.set_surface_material(1, computer_screen_on)
+		computer_powered_up = true
+
+
+func item_used_on_environment(inventory_item, environment_item):
+	if inventory_item == "floppy_disk" and environment_item == "computer":
+		if not computer_powered_up:
+			return
+		if $Desk/Monitor/InteractiveScreen.visible:
+			return
+		$AnimationPlayer.play("insert_floppy_disk")
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	$Desk/Monitor/InteractiveScreen.show()
+	demo.emit_signal("program_activated")
