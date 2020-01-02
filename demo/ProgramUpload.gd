e20f38cf23fbef6d16f958f631f17fc853cea1c9
Paweł Fertyk <pfertyk@pfertyk.me>
2020-01-02 15:09:09 +0100

    Show 2 buttons for upload program

--- demo/ProgramUpload.gd
+++ demo/ProgramUpload.gd
@@ -1,16 +1,37 @@
 extends Control
 
+onready var screen_progress = $Background/ProgressBar
+onready var screen_upload = $Background/Upload
+onready var screen_why_not = $Background/WhyNot
+onready var screen_done = $Background/Done
+onready var timer = $Timer
+onready var animation_player = $AnimationPlayer
+
 
 func _ready():
-	demo.connect("program_activated", $AnimationPlayer, "play", ["upload"])
-
-
-func _on_Upload_pressed():
-	demo.emit_signal("program_uploaded")
-	$ColorRect/Upload.hide()
-	$ColorRect/Done.show()
+	demo.connect("program_activated", animation_player, "play", ["upload"])
+	# For testing purpose
+	if owner == null:
+		animation_player.play("upload")
 
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
-	$ColorRect/ProgressBar.hide()
-	$ColorRect/Upload.show()
+	screen_progress.hide()
+	screen_upload.show()
+
+
+func _on_Yes_pressed():
+	demo.emit_signal("program_uploaded")
+	screen_upload.hide()
+	screen_done.show()
+
+
+func _on_No_pressed():
+	screen_upload.hide()
+	screen_why_not.show()
+	timer.start()
+
+
+func _on_Timer_timeout():
+	screen_why_not.hide()
+	screen_upload.show()
