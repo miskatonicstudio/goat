306bfb24372709d1d2c73471b1f6d94f5b6eee28
Paweł Fertyk <pfertyk@pfertyk.me>
2020-01-05 01:21:13 +0100

    Add key bindings to settings

--- goat/settings/SettingsSlider.gd
+++ goat/settings/SettingsSlider.gd
@@ -4,6 +4,8 @@ extends VBoxContainer
 export (String) var label_text = "" setget set_label_text
 export (String) var settings_section
 export (String) var settings_key
+export (float) var min_value
+export (float) var max_value
 
 onready var slider = $Slider
 onready var label = $Label
@@ -11,6 +13,10 @@ onready var label = $Label
 
 func _ready():
 	label.text = label_text
+	if min_value:
+		slider.min_value = min_value
+	if max_value:
+		slider.max_value = max_value
 	if settings_section and settings_key:
 		slider.value = goat.settings.get_value(settings_section, settings_key)
 
