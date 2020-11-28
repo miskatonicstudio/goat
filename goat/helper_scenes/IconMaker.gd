681bcbe36b2cbec767e87ad77a973c479f9b6931
Paweł Fertyk <pfertyk@pfertyk.me>
2020-11-29 00:56:34 +0100

    Fix a bug with duplicated script

--- goat/helper_scenes/IconMaker.gd
+++ goat/helper_scenes/IconMaker.gd
@@ -4,7 +4,7 @@ onready var viewport = $Viewport
 onready var pivot = $Viewport/Spatial/Pivot
 
 
-func _process(delta):
+func _process(_delta):
 	if goat.ENABLE_INVENTORY_ICON_ROTATION:
 		var seconds = OS.get_ticks_msec() * 0.001
 		pivot.rotation_degrees.y = (
@@ -14,6 +14,7 @@ func _process(delta):
 
 func make_icon_texture(model_scene_path):
 	var scene = load(model_scene_path).instance()
+	scene.set_script(null)
 	for c in pivot.get_children():
 		pivot.remove_child(c)
 	pivot.add_child(scene)
