11cbf5e811afc7af3fe59ac113a9205fba29a7e5
Paweł Fertyk <pfertyk@pfertyk.me>
2021-11-21 23:19:29 +0100

    Show inventory bar on replace item signal

--- goat/helper_scenes/InventoryBar.gd
+++ goat/helper_scenes/InventoryBar.gd
@@ -11,12 +11,14 @@ onready var items = $Items
 func _ready():
 	goat.connect("game_mode_changed", self, "_on_game_mode_changed")
 	goat_inventory.connect("item_added", self, "_on_item_added")
+	goat_inventory.connect("item_replaced", self, "_on_item_replaced")
 	goat_inventory.connect("items_changed", self, "_on_items_changed")
 
 
-func _on_game_mode_changed(_new_game_mode):
-	animation_player.stop(true)
-	hide()
+func _on_game_mode_changed(new_game_mode):
+	if new_game_mode != goat.GameMode.EXPLORING:
+		animation_player.stop(true)
+		hide()
 
 
 func _on_items_changed(new_items):
@@ -29,8 +31,12 @@ func _on_items_changed(new_items):
 			item_button.icon = null
 
 
+func _on_item_replaced(_replaced_item_name, _replacing_item_name):
+	_on_item_added(_replacing_item_name)
+
+
 func _on_item_added(_item_name):
-	if goat.game_mode == goat.GameMode.EXPLORING:
+	if goat.game_mode == goat.GameMode.EXPLORING or goat.game_mode == goat.GameMode.CONTEXT_INVENTORY:
 		show()
 		if animation_player.is_playing():
 			var progress = SLIDE_TIME * (
