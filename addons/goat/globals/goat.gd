6dbb24e7cff0cd5ad02b72a6845c08fa6bad335b
Paweł Fertyk <pfertyk@pfertyk.me>
2023-04-27 00:57:35 +0200

    Add per-game dir for screenshots

--- addons/goat/globals/goat.gd
+++ addons/goat/globals/goat.gd
@@ -12,6 +12,8 @@ enum GameMode {
 }
 
 export (GameMode) var game_mode = GameMode.NONE setget set_game_mode
+# TODO: make this read-only
+var game_directory_path = null
 
 
 func set_game_mode(new_game_mode):
@@ -27,8 +29,16 @@ func _input(_event):
 		take_screenshot()
 
 
+func _setup_game_directory():
+	var path_prefix = RegEx.new()
+	path_prefix.compile("^(user|res)://")
+	var game_directory_name = path_prefix.sub(GAME_RESOURCES_DIRECTORY, "")
+	game_directory_path = "user://" + game_directory_name + "/"
+	Directory.new().make_dir(game_directory_path)
+
+
 func take_screenshot() -> void:
-	var screenshot_directory_path = "user://" + SCREENSHOT_DIRECTORY
+	var screenshot_directory_path = game_directory_path + SCREENSHOT_DIRECTORY
 	Directory.new().make_dir(screenshot_directory_path)
 	var dt = OS.get_datetime()
 	var screenshot_filename = "Screenshot %04d-%02d-%02d %02d:%02d:%02d.png" % [
@@ -42,7 +52,9 @@ func take_screenshot() -> void:
 
 
 func load_game(game_directory: String):
+	assert(game_directory != null)
 	goat.GAME_RESOURCES_DIRECTORY = game_directory
+	_setup_game_directory()
 	goat_voice.load_all()
 	goat_inventory.load_all()
 	goat_state.load_all()
@@ -51,6 +63,8 @@ func load_game(game_directory: String):
 
 
 func clear_game():
+	goat.GAME_RESOURCES_DIRECTORY = null
+	game_directory_path = null
 	goat_globals.reset()
 	goat_inventory.reset()
 	goat_state.reset()
