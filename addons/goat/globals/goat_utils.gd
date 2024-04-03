dc962e2c743209d1032ec2594a46e33dbd0bac81
Paweł Fertyk <pfertyk@pfertyk.me>
2024-04-03 20:05:18 +0200

    Use `get_files` instead of `list_dir_begin`
    Fixes #324

--- addons/goat/globals/goat_utils.gd
+++ addons/goat/globals/goat_utils.gd
@@ -2,18 +2,8 @@ extends Node
 
 
 func list_directory(path):
-	var files = []
 	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin() # TODOGODOT4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir():
-				files.append(file_name)
-			file_name = dir.get_next()
-	else:
-		print("An error occurred when trying to access the path: ", path)
-	return files
+	return dir.get_files()
 
 
 func load_text_file(path):
