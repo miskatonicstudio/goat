de0096f4feb916822c68ee04a6991bfeff42b2d7
Miskatonic Studio <36821133+miskatonicstudio@users.noreply.github.com>
2024-12-18 21:39:55 +0100

    Handle missing GOAT resources (#340)
    Currently, GOAT depends on a certain directory structure whenever
    `game_resources_directory` is configured. If the required directories
    are not present, GOAT will crash. This commit fixes it.
    
    Co-authored-by: Paweł Fertyk <pfertyk@pfertyk.me>

--- addons/goat/helper_scenes/Settings.gd
+++ addons/goat/helper_scenes/Settings.gd
@@ -5,9 +5,12 @@ func _ready():
 	goat.connect("game_mode_changed", self._on_game_mode_changed)
 	
 	var settings_scene_path = "res://addons/goat/default/Settings.tscn"
+	# Check if a custom settings scene is available
 	var game_resources_directory = goat.get_game_resources_directory()
 	if game_resources_directory:
-		settings_scene_path = game_resources_directory + "/goat/scenes/Settings.tscn"
+		var custom_settings_scene_path = game_resources_directory + "/goat/scenes/Settings.tscn"
+		if ResourceLoader.exists(custom_settings_scene_path):
+			settings_scene_path = custom_settings_scene_path
 	var settings_scene = load(settings_scene_path).instantiate()
 	self.add_child(settings_scene)
 
